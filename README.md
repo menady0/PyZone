80c8c84962029116092abc5757a5df9d87430445
Mena-Nady <menady20@gmail.com>
2024-04-08 20:08:37 +0200

    Adding the links to download

--- README.md
+++
@@ -1,2 +0,0 @@
-# PyZone
-This is the website for PyZone Project which is a website to download the educational software to learn the fundamentals of python.
--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-document.addEventListener("mousemove", function (e) {
-  let body = document.querySelector("body");
-  let star = document.createElement("span");
-  star.setAttribute("id", "stars");
-
-  let size = Math.random() * 40;
-  let transformValue = Math.random() * 360;
-
-  star.style.left = e.offsetX + "px";
-  star.style.top = e.offsetY + "px";
-  star.style.fontSize = 10 + size + "px";
-  star.style.transform = "rotate(" + transformValue + "deg)";
-
-  body.appendChild(star);
-
-  setTimeout(function () {
-    star.remove();
-  }, 800);
-});
-
-
-let span = document.querySelector(".text p span");
+let span = document.querySelector(".context div p span.typing");
 let texts = ["Learn", "Enjoy"];
 let indexWord = 0;
 let indexChar = 0;
 let isDeleting = false;
-const type = 150, remove = 75, hold = 1200;
+const type = 150,
+  remove = 75,
+  hold = 1200;
 
 const typing = () => {
   const currentWord = texts[indexWord];
@@ -40,24 +21,9 @@ const typing = () => {
     setTimeout(typing, remove);
   } else {
     isDeleting = !isDeleting;
-  span.classList.remove("stop-blink");
+    span.classList.remove("stop-blink");
     indexWord = !isDeleting ? (indexWord + 1) % texts.length : indexWord;
     setTimeout(typing, hold);
   }
 };
 typing();
-
-
-function scrollToSection(sectionId) {
-    var targetElement = document.getElementById(sectionId);
-    if (targetElement) {
-        // Change the hash in the URL
-        window.location.hash = sectionId;
-        // Perform the smooth scroll
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-    }
-}
-
-// Event listeners for labels
-document.querySelector('label[for="home0"]').onclick = function() { scrollToSection('home'); };
-//document.querySelector('label[for="profile"]').onclick = function() { scrollToSection('targetSection'); };
